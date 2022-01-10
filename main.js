21a6455eefcbaa606a515a6d663b0f59eabb3907
Thanh Nhan <nhan472569@gmail.com>
2022-01-10 20:37:29 +0700

    add play music options

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const song = document.querySelector('#song');
+const thumb = document.querySelector('.thumb');
+const musicName = document.querySelector('.music-name');
+const authorName = document.querySelector('.author-name');
+
 const playBtn = document.querySelector('.play-inner');
 const nextBtn = document.querySelector('.play-forward');
 const prevBtn = document.querySelector('.play-back');
+const infiniteBtn = document.querySelector('.play-infinite');
+const repeatBtn = document.querySelector('.play-repeat');
 
 const currentTimeEl = document.querySelector('.current-time');
 const durationTimeEl = document.querySelector('.duration-time');
@@ -9,15 +15,54 @@ const rangeTimeEl = document.querySelector('.music-range');
 
 let musicIndex = 0;
 let isPLaying = false;
+let behavior = 'repeat';
+
+infiniteBtn.addEventListener('click', () => {
+  infiniteBtn.classList.add('active');
+  repeatBtn.classList.remove('active');
+  behavior = 'infinite';
+});
+
+repeatBtn.addEventListener('click', () => {
+  infiniteBtn.classList.remove('active');
+  repeatBtn.classList.add('active');
+  behavior = 'repeat';
+});
 
 const musics = [
-  'BadHabits-EdSheeran.mp3',
-  'Happier-EdSheeran.mp3',
-  'Perfect-EdSheeran.mp3',
-  'ShapeOfYou-EdSheeran.mp3',
+  {
+    filename: 'BadHabits-EdSheeran.mp3',
+    name: 'Bad Habits',
+    singer: 'Ed sheeran',
+    image: './images/badhabits.jpg',
+  },
+  {
+    filename: 'Happier-EdSheeran.mp3',
+    name: 'Happier',
+    singer: 'Ed sheeran',
+    image: './images/happier.jpg',
+  },
+  {
+    filename: 'Perfect-EdSheeran.mp3',
+    name: 'Perfect',
+    singer: 'Ed sheeran',
+    image: './images/perfect.jpg',
+  },
+  {
+    filename: 'ShapeOfYou-EdSheeran.mp3',
+    name: 'Shape Of You',
+    singer: 'Ed sheeran',
+    image: './images/shapeofyou.jpg',
+  },
 ];
 
-song.src = `./musics/${musics[musicIndex]}`;
+const playerInit = () => {
+  song.src = `./musics/${musics[musicIndex].filename}`;
+  thumb.src = musics[musicIndex].image;
+  musicName.textContent = musics[musicIndex].name;
+  authorName.textContent = musics[musicIndex].singer;
+};
+playerInit();
 
 const rangeChangeHandler = () => {
   song.currentTime = rangeTimeEl.value;
@@ -56,7 +101,7 @@ const changeMusic = direction => {
     if (musicIndex > musics.length - 1) {
       musicIndex = 0;
     }
-    song.src = `./musics/${musics[musicIndex]}`;
+    playerInit();
     isPLaying = false;
   }
   if (direction === -1) {
@@ -64,7 +109,7 @@ const changeMusic = direction => {
     if (musicIndex < 0) {
       musicIndex = musics.length - 1;
     }
-    song.src = `./musics/${musics[musicIndex]}`;
+    playerInit();
     isPLaying = false;
   }
 
@@ -95,6 +140,17 @@ nextBtn.addEventListener('click', () => {
   changeMusic(1);
 });
 
+const replay = () => {
+  playerInit();
+};
+
 song.addEventListener('ended', () => {
-  changeMusic(1);
+  if (behavior === 'repeat') {
+    changeMusic(1);
+  }
+  if (behavior === 'infinite') {
+    rangeTimeEl.value = 0;
+    isPLaying = false;
+    playPause();
+  }
 });
