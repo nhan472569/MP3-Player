e9d332cd478d59fd0509d4a54da08c38872e1284
Thanh Nhan <nhan472569@gmail.com>
2022-01-10 17:55:26 +0700

    timer and range function

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ const playBtn = document.querySelector('.play-inner');
 const nextBtn = document.querySelector('.play-forward');
 const prevBtn = document.querySelector('.play-back');
 
+const currentTimeEl = document.querySelector('.current-time');
+const durationTimeEl = document.querySelector('.duration-time');
+const rangeTimeEl = document.querySelector('.music-range');
+
+let musicIndex = 0;
+let isPLaying = false;
+
 const musics = [
   'BadHabits-EdSheeran.mp3',
   'Happier-EdSheeran.mp3',
@@ -10,9 +17,38 @@ const musics = [
   'ShapeOfYou-EdSheeran.mp3',
 ];
 
-let musicIndex = 0;
+song.src = `./musics/${musics[musicIndex]}`;
 
-let isPLaying = false;
+const rangeChangeHandler = () => {
+  song.currentTime = rangeTimeEl.value;
+};
+
+rangeTimeEl.addEventListener('change', rangeChangeHandler);
+
+const updateTime = () => {
+  const { currentTime, duration } = song;
+  rangeTimeEl.max = duration;
+  rangeTimeEl.value = currentTime;
+  if (currentTime) {
+    currentTimeEl.textContent = formatTime(currentTime);
+  } else {
+    currentTimeEl.textContent = '00:00';
+  }
+  durationTimeEl.textContent = formatTime(duration);
+};
+
+const formatTime = number => {
+  const minutes = Math.floor(number / 60);
+  const secconds = Math.floor(number - minutes * 60);
+
+  return `${minutes < 10 ? minutes.toString().padStart(2, 0) : minutes} : ${
+    secconds < 10 ? secconds.toString().padStart(2, 0) : secconds
+  }`;
+};
+
+let timer = setInterval(() => {
+  updateTime();
+}, 500);
 
 const changeMusic = direction => {
   if (direction === 1) {
@@ -39,9 +75,13 @@ const playPause = () => {
   if (isPLaying) {
     song.pause();
     playBtn.innerHTML = '<ion-icon name="play"></ion-icon>';
+    clearInterval(timer);
   } else {
     song.play();
     playBtn.innerHTML = '<ion-icon name="pause"></ion-icon>';
+    timer = setInterval(() => {
+      updateTime();
+    }, 500);
   }
   isPLaying = !isPLaying;
 };
@@ -52,5 +92,9 @@ prevBtn.addEventListener('click', () => {
   changeMusic(-1);
 });
 nextBtn.addEventListener('click', () => {
+  changeMusic(1);
+});
+
+song.addEventListener('ended', () => {
   changeMusic(1);
 });
